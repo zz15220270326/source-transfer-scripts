1579dbe5a0c78862c75e723fb21f14ea617be607
zz15220270326 <826709675@qq.com>
2023-10-08 17:44:20 +0800

    feat(video-source-transfer): 新增 chunk audioTransfer & 修改 chunk videoTransfer 初始化逻辑

---
+++ src/scripts/audioTransfer.ts
@@ -0,0 +1,12 @@
+import '../assets/css/reset.scss';
+import '../assets/css/audioTransfer.scss';
+
+import { getHtmlElement } from '../libs/utils';
+
+;(() => {
+  const init = () => {
+
+  }
+
+  init();
+})();
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,14 +3,17 @@ import '../assets/css/index.scss';
 
 import { getHtmlElement } from '../libs/utils';
 import { VideoListTable, VideoListPagination } from '../modules/index';
+import DomModule from '../modules/DomModule';
 
 ;(() => {
   const oVideoListTable: HTMLElement | null = getHtmlElement('.J_VideoListTable');
   const oVideoListPagination: HTMLElement | null = getHtmlElement('.pagination-container');
 
   const init = () => {
-    oVideoListTable && new VideoListTable(oVideoListTable).init();
-    oVideoListPagination && new VideoListPagination(oVideoListPagination).init();
+    DomModule.init([
+      new VideoListTable(oVideoListTable),
+      new VideoListPagination(oVideoListPagination)
+    ]);
   }
 
   init();
