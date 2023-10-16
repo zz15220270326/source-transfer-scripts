5d9cdf9e26b4af63185ed9be6ebfbf62bb762147
zz15220270326 <826709675@qq.com>
2023-10-16 19:27:24 +0800

    feat(source-transfer-scripts): 初始化 detail 配置

---
+++ src/scripts/detail.ts
@@ -0,0 +1,17 @@
+import '../assets/css/reset.scss';
+import '../assets/css/detail.scss';
+
+import { getHtmlElement } from '../libs/utils';
+
+import DomModule from '../modules/DomModule';
+
+;(() => {
+
+  const init = () => {
+    DomModule.init([
+    ]);
+  }
+
+  init();
+})();
+
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,7 +2,7 @@ import '../assets/css/reset.scss';
 import '../assets/css/index.scss';
 
 import { getHtmlElement } from '../libs/utils';
-;
+
 import DomModule from '../modules/DomModule';
 
 ;(() => {
