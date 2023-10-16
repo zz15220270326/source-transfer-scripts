13e59c6a6cfa3de5d4315d02ac60d95bee8557da
zz15220270326 <826709675@qq.com>
2023-10-16 22:03:49 +0800

    feat(source-transfer-scripts): 添加 detail 页面样式 & video.js

--- src/scripts/detail.ts
+++ src/scripts/detail.ts
@@ -1,12 +1,13 @@
 import '../assets/css/reset.scss';
 import '../assets/css/detail.scss';
+import 'video.js/dist/video-js.min.css';
+// import VideoJS from 'video.js';
 
 import { getHtmlElement } from '../libs/utils';
 
 import DomModule from '../modules/DomModule';
 
 ;(() => {
-
   const init = () => {
     DomModule.init([
     ]);
